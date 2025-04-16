949ab92f6e9a3bc727a0b89acffb556f0bbfbdc1
MaximoLagos <mlagosmarmol1@mail.austral.edu.ar>
2025-04-15 22:41:18 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,29 +1,31 @@
 def line():
-    A = input("Ingrese el coeficiente A: ")
-    B = input("Ingrese el coeficiente B: ")
-    X1 = input("Ingrese el coeficiente X1: ")
-    X2 = input("Ingrese el coeficiente X2: ")
+ 	A = input("Ingrese el coeficiente A: ")
+ 	B = input("Ingrese el coeficiente B: ")
+ 	X1 = input("Ingrese el coeficiente X1: ")
+ 	X2 = input("Ingrese el coeficiente X2: ")
 
-    print("El coeficiente A de su ecuación de la recta es: " + str(float(A)))
-    print("El coeficiente B de su ecuación de la recta es: " + str(float(B)))
-    print("El coeficiente X1 de su ecuación de la recta es: " + str(float(X1)))
-    print("El coeficiente X2 de su ecuación de la recta es: " + str(float(X2)))
+ 	print("El coeficiente A de su ecuación de la recta es: " + format(float(A), '.1f'))
+ 	print("El coeficiente B de su ecuación de la recta es: " + format(float(B), '.1f'))
+ 	print("El coeficiente X1 de su ecuación de la recta es: " + format(float(X1), '.1f'))
+ 	print("El coeficiente X2 de su ecuación de la recta es: " + format(float(X2), '.1f'))
 
-    A = float(A)
-    B = float(B)
-    X1 = float(X1)
-    X2 = float(X2)
+ 	A = float(A)
+ 	B = float(B)
+ 	X1 = float(X1)
+ 	X2 = float(X2)
 
-    print(f"\nPara la siguiente ecuación:\n\tY = {A}X + {B}")
+ 	print()
+ 	print("Para la siguiente ecuación:")
+ 	print("\tY = {}X + {}".format(A, B))
 
-    Y1 = (A * X1) + B
-    Y2 = (A * X2) + B
+ 	Y1 = A * X1 + B
+ 	Y2 = A * X2 + B
 
-    print(f"\nDados los siguientes puntos:")
-    print(f"\tP1 = ({X1}, {Y1})")
-    print(f"\tP2 = ({X2}, {Y2})")
+ 	print()
+ 	print("Dados los siguientes puntos:")
+ 	print("\tP1 ({}, {})".format(X1, Y1))
+ 	print("\tP2 ({}, {})".format(X2, Y2))
 
-    D = ((X2 - X1) ** 2 + (Y2 - Y1) ** 2) ** 0.5
-
-    print("\nLa distancia entre ellos es: " + str(D))
-    
+ 	D = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
+ 	print()
+ 	print("La distancia entre ellos es: {}".format(D))
